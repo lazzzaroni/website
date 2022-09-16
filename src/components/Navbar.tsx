230de47c93ab6f1426aa87954f7cd24152bb07b9
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-09-16 14:20:26 +0200

    feat: add MobileMenu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,8 @@ import dynamic from "next/dynamic";
 import Link from "next/link";
 import { NextRouter, useRouter } from "next/router";
 
+import MobileMenu from "./MobileMenu";
+
 const ColorModeToggle = dynamic(() => import("@/components/ColorModeToggle"), {
   ssr: false,
 });
@@ -19,9 +21,9 @@ const NavItem: FC<{ href: string; text: string; router: NextRouter }> = ({
     <Link href={href === "/home" ? "/" : href}>
       <a
         className={clsx(
-          "hidden md:inline-block p-1 sm:px-2 sm:py-1 rounded-lg mx-1 hover:bg-slate-200 dark:hover:bg-zinc-800 transition-all font-medium",
+          "hidden sm:inline-block px-3 py-2 mr-1 rounded-full hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-all font-medium",
           isActive
-            ? "text-purple-500 dark:text-purple-400 font-bold"
+            ? "bg-transparent hover:bg-transparent dark:hover:bg-zinc-900 text-purple-700 dark:text-purple-400"
             : "hover:text-inherit"
         )}
       >
@@ -36,11 +38,12 @@ const Navbar = () => {
   const links = ["home", "blog", "projects", "about"];
 
   return (
-    <nav className="relative flex items-center justify-between w-full max-w-2xl py-8 mx-auto capitalize border-zinc-200 dark:border-zinc-700 sm:pb-16 text-zinc-900 dark:text-zinc-100">
+    <nav className="flex items-center justify-between w-full py-6 mx-auto capitalize border-zinc-200 dark:border-zinc-700 sm:pb-16 text-zinc-900 dark:text-zinc-100">
       <a href="#skip" className="skip-nav">
         Skip to content
       </a>
-      <div className="ml-[-0.60rem]">
+      <div>
+        <MobileMenu />
         {links.map((link, index) => (
           <NavItem href={`/${link}`} text={link} router={router} key={index} />
         ))}
